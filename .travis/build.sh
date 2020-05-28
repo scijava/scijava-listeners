d5e431c0b0f75c2bc3198e33de1035571ecaca6a
tpietzsch <tobias.pietzsch@gmail.com>
2020-05-28 23:45:29 +0200

    Travis: update .travis/build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh $encrypted_5d86febf649a_key $encrypted_5d86febf649a_iv
+sh travis-build.sh
